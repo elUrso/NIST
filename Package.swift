8ca3754cbdff5b17e62af2ab97be61ae3db410cc
Vitor Silva <sirmochi45@gmail.com>
2019-07-04 12:06:05 -0300

    remove library from manifest

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "NIST",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "NIST",
-            targets: ["NIST"]),
-    ],
+    // products: [
+    //     // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    //     .library(
+    //         name: "NIST",
+    //         targets: ["NIST"]),
+    // ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
